3597efa61ed91ed65aa81b40f19a76de19086b80
Katie Escoto <katie.escoto@gmail.com>
2020-07-07 15:47:54 -0400

    fixed User references in auth, api, index files

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router()
-const User = require('../db/models/user')
+const Teacher = require('../db/models/teacher')
 module.exports = router
 
 router.post('/login', async (req, res, next) => {
   try {
-    const user = await User.findOne({where: {email: req.body.email}})
+    const user = await Teacher.findOne({where: {email: req.body.email}})
     if (!user) {
       console.log('No such user found:', req.body.email)
       res.status(401).send('Wrong username and/or password')
@@ -21,11 +21,11 @@ router.post('/login', async (req, res, next) => {
 
 router.post('/signup', async (req, res, next) => {
   try {
-    const user = await User.create(req.body)
+    const user = await Teacher.create(req.body)
     req.login(user, err => (err ? next(err) : res.json(user)))
   } catch (err) {
     if (err.name === 'SequelizeUniqueConstraintError') {
-      res.status(401).send('User already exists')
+      res.status(401).send('Teacher already exists')
     } else {
       next(err)
     }
