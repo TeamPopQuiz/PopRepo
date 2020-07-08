96aa92bdf28f7fa29a679a760345ef35decf3520
Jackie Francis <jayc.francis2@gmail.com>
2020-07-08 12:48:14 -0400

    added conditional student-teacher routes to route component

--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
+import {Login, Signup, UserHome, TeacherHome} from './components'
 import {me} from './store'
 
 /**
@@ -21,12 +21,15 @@ class Routes extends Component {
         {/* Routes placed here are available to all visitors */}
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        {isLoggedIn && (
-          <Switch>
-            {/* Routes placed here are only available after logging in */}
+        {isLoggedIn &&
+          (this.props.role === 'teacher' ? (
+            <Switch>
+              {/* Routes placed here are only available after logging in */}
+              <Route path="/home" component={TeacherHome} />
+            </Switch>
+          ) : (
             <Route path="/home" component={UserHome} />
-          </Switch>
-        )}
+          ))}
         {/* Displays our Login component as a fallback */}
         <Route component={Login} />
       </Switch>
@@ -41,7 +44,8 @@ const mapState = state => {
   return {
     // Being 'logged in' for our purposes will be defined has having a state.user that has a truthy id.
     // Otherwise, state.user will be an empty object, and state.user.id will be falsey
-    isLoggedIn: !!state.user.id
+    isLoggedIn: !!state.user.id,
+    role: state.user.role
   }
 }
 
