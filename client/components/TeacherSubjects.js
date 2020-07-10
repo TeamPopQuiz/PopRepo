59737cd3633c8a2d0ec627f4f752a3dac4c39eb8
Jackie Francis <jayc.francis2@gmail.com>
2020-07-10 10:21:50 -0400

    All subjects page lists subjects associated with teacher

--- client/components/TeacherSubjects.js
+++ client/components/TeacherSubjects.js
@@ -1,18 +1,35 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import {connect} from 'react-redux'
 import AddRoom from './AddRoom'
 import {getSubjects} from '../store/subject'
 
-const TeacherSubjects = props => {
-  useEffect(() => {
-    props.getRooms()
-  })
-  return (
-    <div>
-      <h1>My Subjects</h1>
-      <AddRoom />
-    </div>
-  )
+class TeacherSubjects extends React.Component {
+  componentDidMount() {
+    this.props.getRooms()
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>My Subjects</h1>
+        <AddRoom />
+        <h2>Existing subjects</h2>
+        <ul>
+          {this.props.subjects && this.props.subjects.length ? (
+            this.props.subjects.map(subject => {
+              return (
+                <li key={subject.id}>
+                  {subject.name} {subject.roomCode}
+                </li>
+              )
+            })
+          ) : (
+            <li>No Subjects</li>
+          )}
+        </ul>
+      </div>
+    )
+  }
 }
 
 const mapDispatch = dispatch => {
@@ -21,4 +38,10 @@ const mapDispatch = dispatch => {
   }
 }
 
-export default connect(null, mapDispatch)(TeacherSubjects)
+const mapState = state => {
+  return {
+    subjects: state.subjects
+  }
+}
+
+export default connect(mapState, mapDispatch)(TeacherSubjects)
