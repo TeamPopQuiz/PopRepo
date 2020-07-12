23336885f32206ed9e982edb2e8d1771008834d1
edeniz9191@gmail.com <edeniz9191@gmail.com>
2020-07-12 19:46:02 -0400

    added nav bar.  nav bar is not linked properly.  the rest of the page is not centered.

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,33 +1,128 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
+// import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import {makeStyles} from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import Link from '@material-ui/core/Link'
+import Drawer from '@material-ui/core/Drawer'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import List from '@material-ui/core/List'
+import Divider from '@material-ui/core/Divider'
+import ListItem from '@material-ui/core/ListItem'
+import ListItemText from '@material-ui/core/ListItemText'
 
-const Navbar = ({handleClick, isLoggedIn, role}) => (
-  <div>
-    <h1>MindPop!</h1>
-    <nav>
+const drawerWidth = 240
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex'
+  },
+  title: {
+    flexGrow: 1
+  },
+  appBar: {
+    zIndex: theme.zIndex.drawer + 1
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0
+  },
+  drawerPaper: {
+    width: drawerWidth
+  },
+  drawerContainer: {
+    overflow: 'auto'
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3)
+  }
+}))
+
+const Navbar = ({handleClick, isLoggedIn, role}) => {
+  const classes = useStyles()
+  return (
+    <div>
+      <h1>MindPop!</h1>
+
       {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-          {role === 'teacher' ? <Link to="/all-subjects">Subjects</Link> : null}
+        <div className={classes.root}>
+          <CssBaseline />
+          <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar>
+              <Typography variant="h6" noWrap>
+                MindPop{' '}
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            classes={{
+              paper: classes.drawerPaper
+            }}
+          >
+            <Toolbar />
+            <div className={classes.drawerContainer}>
+              <List>
+                <ListItem button key="Home">
+                  <ListItemText primary="Home" />
+                </ListItem>
+              </List>
+              <Divider />
+              <List>
+                {[
+                  'View Classroom',
+                  'Create Classroom',
+                  'Edit Students',
+                  'Class Data'
+                ].map((text, index) => (
+                  <ListItem button key={text}>
+                    <ListItemText primary={text} />
+                  </ListItem>
+                ))}
+              </List>
+              <Divider />
+              <List>
+                {['Profile', 'Logout'].map((text, index) => (
+                  <ListItem button key={text}>
+                    <ListItemText primary={text} />
+                  </ListItem>
+                ))}
+              </List>
+            </div>
+          </Drawer>
         </div>
       ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
+        <nav>
+          <div className={classes.root}>
+            <AppBar position="static">
+              <Toolbar>
+                <Button color="inherit">
+                  <Link href="/login" color="inherit">
+                    Login
+                  </Link>
+                </Button>
+
+                <Button color="inherit">
+                  <Link href="/signup" color="inherit">
+                    Sign Up
+                  </Link>
+                </Button>
+              </Toolbar>
+            </AppBar>
+          </div>
+        </nav>
       )}
-    </nav>
-    <hr />
-  </div>
-)
+    </div>
+  )
+}
 
 /**
  * CONTAINER
