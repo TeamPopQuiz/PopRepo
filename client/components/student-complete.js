98095fca096d3ae3548309f66cf661de33e7f00b
Lauren Menzies <laurenmenzies@gmail.com>
2020-07-13 11:19:23 -0400

    React Student Home + Complete

---
+++ client/components/student-complete.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+
+class StudentComplete extends Component {
+  render() {
+    return (
+      <div>
+        <h3>CONGRATULATIONS!</h3>
+        <p>You have completed your MindPop!</p>
+        <p>SMILE IMAGE PLACEHOLDER</p>
+      </div>
+    )
+  }
+}
+
+export default StudentComplete
