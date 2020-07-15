f7023a80ea0d4ee129fb350053f6aeaebdd5e118
Lauren Menzies <laurenmenzies@gmail.com>
2020-07-15 15:44:50 -0400

    Homepage

--- client/routes.js
+++ client/routes.js
@@ -32,7 +32,8 @@ class Routes extends Component {
     return (
       <Switch>
         {/* Routes placed here are available to all visitors */}
-        <Route path="/" component={Homepage} />
+        <Route exact path="/" component={Homepage} />
+        <Route path="/home" component={Homepage} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
         {isLoggedIn &&
